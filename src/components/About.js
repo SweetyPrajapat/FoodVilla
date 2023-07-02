621749222c6a596ded28cece3b6172b0d1f44e07
SweetyPrajapat <luhaniwalsweety@gmail.com>
2023-07-02 16:37:00 +0530

    modified About us

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Profile from "./Profile";
+
+import avif from "../assets/img/logo.avif";
 import ProfileCls from "./ProfileClass";
 
 const About = () => {
   return (
-    <div>
-      <h1>About us page</h1>
-      <p>This is namaste react live coures</p>
+    <div className="text-center my-3">
+      <h1 className="text-2xl text-purple-800">About us </h1>
+      <div className="mx-96 px-24 py-4 my-0">
+        <img className="h-72 " src={avif} alt="Profile alt" />
+      </div>
       {<Profile name={"Sweety"} />}
-      {<ProfileCls name={"Pari"} />}
       <Outlet />
     </div>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,14 @@ const Profile = (props) => {
   const [count, setCount] = useState(1);
   return (
     <div>
-      <h1>Profile component</h1>
-      <h3>Name: {props.name}</h3>
+      <h1 className="my-6">
+        Providing the best Quality food and best Services at your door.
+      </h1>
+
+      <p>We are just a click away.</p>
+      {/* <h3>Name: {props.name}</h3>
       <h4>Count: {count}</h4>
-      <button onClick={() => setCount(2)}>click me</button>
+      <button onClick={() => setCount(2)}>click me</button> */}
     </div>
   );
 };
